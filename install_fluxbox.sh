11040c4f9da6869497ce2ff9bad6410e07f230f2
Kiran Kumar Manku <5748958+kirankumarmanku@users.noreply.github.com>
2020-04-06 15:55:50 +0100

    Update install_fluxbox.sh

--- install_fluxbox.sh
+++ install_fluxbox.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-dnf install fluxbox \
-            gmrun \
-            rxvt-unicode-256color-ml \
-            xterm \
-            chromium-browser \
-            xclip \
-            xpdf \
-	    tigervnc-server \
-	    libXau libXext
+dnf -y install fluxbox 
+dnf -y install tigervnc-server
